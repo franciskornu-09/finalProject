9e8564d38835a668dc30493f0c959e80e63f6b6c
franciskornu-09 <franciskornu@gmail.com>
2016-12-08 21:49:58 +0000

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,20 +1,20 @@
-function contact(){
-	var name = $("#search").val();
+document.getElementById("search").addEventListener("click",findContact);
+
+function findContact(){
+	var options = new ContactFindOptions();
+   options.filter = "";
+   options.multiple = true;
+
+   fields = ["displayName"];
+   navigator.contacts.find(fields, contactfindSuccess, contactfindError, options);
+    
+   function contactfindSuccess(contacts) {
+      for (var i = 0; i < contacts.length; i++) {
+         alert("Display Name = " + contacts[i].displayName);
+      }
+   }
 	
-	function onSuccess(contacts) {
-    alert('Found ' + contacts.length + ' contacts.');
-};
-
-function onError(contactError) {
-    alert('onError!');
-};
-
-// find all contacts with 'Bob' in any name field
-var options      = new ContactFindOptions();
-options.filter   = name;
-options.multiple = true;
-options.desiredFields = [navigator.contacts.fieldType.id];
-options.hasPhoneNumber = true;
-var fields       = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
-navigator.contacts.find(fields, onSuccess, onError, options);
+   function contactfindError(message) {
+      alert('Failed because: ' + message);
+   }
 }
